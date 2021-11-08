34c8ef82aeaea76e6c8802313b432fe4d8cd259e
Leonardo Villalobos <levillalobos@csumb.edu>
2021-11-08 14:16:21 -0800

    Create Objects.py file and refactor.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from AnimationProject import main
+from AnimationProject import display_animation
 
 if __name__ == '__main__':
     # Call the main functions defined within the source code project file
-    main()
+    display_animation()
